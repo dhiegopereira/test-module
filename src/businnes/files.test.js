75c649f5711d717f96735fd40d04f7978fea0c9b
Diego Pereira <dhiego_ti@hotmail.com>
2021-05-16 00:53:11 -0300

    Code coverage with istanbul

--- src/businnes/files.test.js
+++ src/businnes/files.test.js
@@ -3,15 +3,31 @@ const expect = require('chai').expect
 const fs = require('fs')
 const sinon = require("sinon");
 
-describe('reading directory', () => {
+describe('success reading directory', () => {
+  const path = './'
+    afterEach(done => {
+        fs.readdir.restore()
+        done()
+    })
     it('reading directory', () => {
         sinon.stub(fs, 'readdir').callsFake((path, cb) => {
             cb(null, ['file1.txt'])
         })
       
-        return files.readdir('./').then( list => {
+        return files.readdir(path).then( list => {
             expect(list.length).to.equal(1)
-            expect(fs.readdir.getCall(0).args[0]).to.equal('./')
+            expect(fs.readdir.getCall(0).args[0]).to.equal(path)
         })
     })
+    it('fails reading directory', () => {
+        sinon.stub(fs, 'readdir').callsFake((path, cb) => {
+            cb('error')
+        })      
+        return files.readdir(path)
+            .catch( err => {
+              expect(err).to.equal('error')
+              expect(fs.readdir.getCall(0).args[0]).to.equal(path)
+        })
+    })
+
 })
